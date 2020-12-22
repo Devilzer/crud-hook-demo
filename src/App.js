9b0e98830a4d4f04f524a84603073b19546e8fe8
Devilzer <jenadeepak724@gmail.com>
2020-12-23 02:03:28 +0530

    add todo , todo form component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React,{ useState } from "react";
 import './App.css';
 
-function App() {
+// task component
+function Todo({todo,index, completeTodo}){
+  return(
+    <div className="todo">
+      {todo.text}
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  )
+};
+
+
+// imput form
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+};
+
+function App() {
+  const [todos,setTodos] = useState([
+    {
+      text : "Learn react hooks",
+      isComplete : false
+    },
+    {
+      text : "complete the project",
+      isComplete : false
+    },
+    {
+      text : "go to gym",
+      isComplete : false
+    },
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {
+          todos.map((todo,index)=>(
+            <Todo key={index}
+              index={index}
+              todo={todo}
+            />
+            
+          ))
+        }
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
